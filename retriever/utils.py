cac7564072e85108a06f275abdef907e057e4d52
yaldashbz <yaldashabanzadeh@gmail.com>
2022-05-31 21:37:38 +0430

    fix: fix a minor bug

--- retriever/utils.py
+++ retriever/utils.py
@@ -1,6 +1,7 @@
-import itertools
 from collections import defaultdict
 from typing import List
+
+from pre_process import PreProcessor
 
 WIKI_CATEGORIES = [
     'bbc', 'religion', 'sport',
@@ -9,18 +10,12 @@ WIKI_CATEGORIES = [
 ]
 
 
-def get_all_sentences(content: str):
-    return list(itertools.chain(content))
-
-
-def get_all_words(content: str):
-    sentences = get_all_sentences(content)
-    return list(itertools.chain(*sentences))
-
-
 def get_keywords(content: str) -> List[str]:
-    words = get_all_words(content)
+    words = list()
+    for sentence in PreProcessor.tokenize(content):
+        words += sentence
     count = defaultdict(int)
     for word in words:
+        word = word.replace(',', '').replace('.', '').replace('?', '').replace('!', '').replace(';', '')
         count[word] += 1
     return list({k: v for k, v in sorted(count.items(), key=lambda item: item[1])}.keys())[-20:]
